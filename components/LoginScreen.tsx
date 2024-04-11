840b7f2d310a44f4b5b5b49ee8f456491ad71be2
O Desenvolvedor Libertário <odevlibertario@proton.me>
2024-04-11 00:52:53 -0300

    Nostrlivery event processing archtecture, authenticating with a nostr signature and pubkey
    amend

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -5,57 +5,56 @@ import Toast from "react-native-toast-message";
 import { getPublicKey } from 'nostr-tools'
 import { finalizeEvent, verifyEvent, nip19 } from 'nostr-tools'
 
-let sk = nip19.decode('nsec1flqhrsshqu2gyn28kfz6mn8644el4dm3auvr82a0qlq5rsmrwwgsp3z954')
-let pk = getPublicKey(sk.data) // pk is a hex string
-
-let event = finalizeEvent({
-  kind: 1,
-  created_at: Math.floor(Date.now() / 1000),
-  tags: [],
-  content: 'hello',
-}, sk.data)
-
-let isGood = verifyEvent(event)
-
-
 // @ts-ignore
 const LoginScreen = ({navigation}) => {
   const [text, onChangeText] = React.useState('');
-  /*
-  TODO This is the simplest way possible to authenticate but it's not safe.
-  I don't want to sent the nsec to the node, but it was not possible to use nostr-tools
-  to validate that the nsec is valid in react-native so I'm doing this in the backend for now.
-   */
+
   const authenticate = () => {
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        nsec: text,
-        profile: 'company'
-      }),
-    }).then((response) => {
-      if(response.ok) {
-        navigation.navigate('Profile')
-      } else {
-        response.json().then(e => {
-          Toast.show({
-            type: 'error',
-            text1: e['error']
-          });
+    try {
+      let sk = nip19.decode(text)
+
+      let event = finalizeEvent({
+        kind: 1,
+        created_at: Math.floor(Date.now() / 1000),
+        tags: [],
+        content: JSON.stringify({
+          eventType: 'LOGIN',
+          params: {profile: 'company'}
+        }),
+      }, sk.data as Uint8Array)
+
+      fetch('http://localhost:3000/entrypoint', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+      }).then((response) => {
+        if(response.ok) {
+          navigation.navigate('Profile')
+        } else {
+          response.json().then(e => {
+            Toast.show({
+              type: 'error',
+              text1: e['error']
+            });
+          })
+
+        }
+
+      }).catch((error) => {
+        Toast.show({
+          type: 'error',
+          text1: error.message
         })
-        
-      }
-      
-    }).catch((error) => {
+      });
+    } catch (error: any) {
       Toast.show({
         type: 'error',
         text1: error.message
       })
-     });
+    }
   };
 
   return (
